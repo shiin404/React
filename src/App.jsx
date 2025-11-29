15b6540391a9fca4eb48b4f3bfa27d0b3a94304f
shiin404 <akashigameplayyt@gmail.com>
2025-11-29 20:04:54 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,47 @@ import { useState } from "react";
 function App() {
 
   let [text,Settext] = useState('')
+  let [description,Setdes] = useState('')
   let [alltext,AddText] = useState([])
+
   function RemoveTask(element){
     AddText(alltext.filter((t)=> t !== element))
   }
+  function AddTask(){
+    let Alltask = alltext.map(item => (
+      <div>
+        <h1>{item.text}</h1>
+        <h3>{item.description}</h3>
+        <button onClick={() => RemoveTask(item)}>Удалить❌</button>
+      </div>
+    ))
+    return Alltask
+  }
   return(
     <div>
+      <button onClick={() => AddText([])}>Удалить всё</button>
       <h1>Todo приложение</h1>
+
       <input type="text" 
-      value={text}
-       onChange={(event)=> Settext(event.target.value)}/>
+        value={text}
+        onChange={(event)=> Settext(event.target.value)}
+        placeholder="Введите задачу"
+      />
+      <input type="text" 
+        value={description}
+        onChange={(event)=> Setdes(event.target.value)}
+        placeholder="Введите описание"
+      />
+      <button onClick={() => {
+        if (text.trim() == '') return
+        if (description.trim() == '') return
+        AddText([...alltext,{text,description}])
+        Settext('')
+        Setdes('')
+        }}>Создать запись
+      </button>
 
-       <button onClick={() => {
-        AddText([...alltext,text])
-        Settext('')}}>Создать запись</button>
-
-       <div>{alltext.map((item) => 
-      <div> 
-        <h1>{item}</h1>
-        <button onClick={() => RemoveTask(item)}>Удалить</button>
-      </div>
-      )}
-       </div>
+       <div>{AddTask()}</div>
     </div>
   )
 }
